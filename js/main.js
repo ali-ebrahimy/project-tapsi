3e48eff1ca6b77ed27f50d2404b93d2fb204a042
aliebrahimy <ebrahimyali051@gmail.com>
2022-05-26 10:49:11 -0700

    Add Like feature

--- js/main.js
+++ js/main.js
@@ -19,3 +19,28 @@ tabs.forEach((tab) => {
     targeTabContent.classList.add("active");
   });
 });
+// ------------------------------
+const heartI=document.querySelectorAll(".slide__details .slide__icon .fa-heart")
+const numberHeart=document.querySelectorAll(".slide__details .slide__icon .spanHeart")
+const containerHeart=document.querySelectorAll(".slide__details .slide__icon .containerHeart");
+let newNum=0;
+ heartI.forEach((e)=>{
+   e.addEventListener("click",()=>{
+     e.classList.toggle("heart")
+     const classList=e.classList;    
+       numberHeart.forEach((h)=>{
+        if(classList.contains("heart")) {
+          newNum=Number(h.innerHTML)+1;
+          h.innerHTML=newNum;
+        }
+        else{
+          newNum=Number(h.innerHTML)-1;
+          h.innerHTML=newNum;
+        } 
+       })   
+   })
+ })
+  
+
+  
+
